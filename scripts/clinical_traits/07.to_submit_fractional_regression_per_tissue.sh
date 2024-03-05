7f41534176a81dbdc291ea23be48591d176d5f75
José Miguel Ramírez <34416160+JMiguelRamirez@users.noreply.github.com>
2024-03-05 14:22:33 +0100

    Update 07.to_submit_fractional_regression_per_tissue.sh

--- scripts/clinical_traits/07.to_submit_fractional_regression_per_tissue.sh
+++ scripts/clinical_traits/07.to_submit_fractional_regression_per_tissue.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo $1
 echo $2
-Rscript /gpfs/projects/bsc83/Projects/GTEx_v8/Jose/03_Models/18.DSA_per_tissue.R $1 $2
+Rscript /gpfs/projects/bsc83/Projects/GTEx_v8/Jose/03_Models/7.DSA_per_tissue.R $1 $2
 
 
